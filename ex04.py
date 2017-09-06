03cb1d675c165b8cdb5de828a3eb7c62b10538ee
peehscavacini <30813668+peehscavacini@users.noreply.github.com>
2017-09-06 19:39:46 -0300

    Delete ex04.py

--- ex04.py
+++
@@ -1,9 +0,0 @@
-
-def maior_ponta(nums):
-  lista = []
-  for i in range(len(nums)):
-    if nums[0] > nums[-1]:
-      lista.append(nums[0])
-    else:
-      lista.append(nums[-1])
-  return lista
