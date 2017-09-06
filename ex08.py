3b8624a256f8fe21429e2b32d37fa3083823764e
peehscavacini <30813668+peehscavacini@users.noreply.github.com>
2017-09-06 19:40:11 -0300

    Delete ex08.py

--- ex08.py
+++
@@ -1,9 +0,0 @@
-def squirrel_play(temp, is_summer):   
-  maior_temp = 90
-  if is_summer:
-    maior_temp = 100
-
-  if temp >= 60 and temp <= maior_temp:
-    return True
-  else:
-    return False
