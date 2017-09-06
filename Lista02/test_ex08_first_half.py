1194a64bb7a0fca98d782ef7c8e79696a8d347b3
brunobuzzo <30813663+brunobuzzo@users.noreply.github.com>
2017-09-06 19:06:13 -0300

    Add files via upload

---
+++ Lista02/test_ex08_first_half.py
@@ -0,0 +1,26 @@
+# -*- coding: utf-8 -*-
+# Exercícios by Nick Parlante (CodingBat)
+
+# H. first_half
+# seja uma string s
+# retorna a primeira metade da string
+# first_half('WooHoo') -> 'Woo'
+# first_half('HelloThere') -> 'Hello'
+# first_half('abcdef') -> 'abc'
+def first_half(s):
+  qtd = len(s)
+  ttl = s[:qtd / 2]      
+  return ttl
+
+
+def test_ex08():
+  print ('First Half')
+  assert first_half('WooHoo') == 'Woo'
+  assert first_half('HelloThere') == 'Hello'
+  assert first_half('abcdef') == 'abc'
+  assert first_half('ab') == 'a'
+  assert first_half('') == ''
+  assert first_half('0123456789') == '01234'
+  assert first_half('kitten') == 'kit'
+
+
